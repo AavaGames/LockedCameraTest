2dae30afb3d7ca29b9afaa56e227b62abb36edb2
Aava F <theaavagames@gmail.com>
2024-02-05 15:08:09 -0400

    Added targetable class, Added fishnet to player camera controller, Moved camera code from motor to camera controller

--- Assets/PlayerCameraController.cs
+++ Assets/PlayerCameraController.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 using Cinemachine;
 using StarterAssets;
 using TMPro;
+using FishNet.Object;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+using Input = UnityEngine.Input;
+using FishNet;
 
-public class PlayerCameraController : MonoBehaviour
+public class PlayerCameraController : NetworkBehaviour
 {
+
+    public GameObject followCameraPrefab;
+    public GameObject targetCameraPrefab;
+
     public enum SortingType { List, Distance }
 
-    private bool _targetting = false;
-    public CinemachineVirtualCamera defaultCamera;
-    public CinemachineVirtualCamera[] targettingCameras;
-    private int currentTargettingCamera = 0;
+    private bool _targeting = false;
+    public CinemachineVirtualCamera followCamera;
+    public CinemachineVirtualCamera[] targetingCameras;
+    private int currentTargetingCamera = 0;
 
-    private ThirdPersonController controller;
+    private PredictionMotor controller;
 
-    public GameObject currentTarget;
-    public List<GameObject> possibleTargets = new List<GameObject>();
+    public Targetable currentTarget;
+    public List<Targetable> possibleTargets = new List<Targetable>();
     private int _targetIndex = -1;
 
     public UnityEngine.UI.Image Reticle;
@@ -26,51 +36,133 @@ public class PlayerCameraController : MonoBehaviour
     private float _targetDistance = 0f;
     public float TargetDistance => _targetDistance;
 
+    private Targetable myTarget;
+    private StarterAssetsInputs _input;
+    private PlayerInput _playerInput;
+
+
     [Header("Variables")]
     public SortingType sortingType;
     [Tooltip("How high to position the camera above the player's feet/origin")]
     public float playerHeightOffset = 1.375f;
     public float cameraDistance = 3.0f;
 
-    void Start()
+    // cinemachine
+    [Header("Cinemachine")]
+    [Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
+    public GameObject CinemachineCameraTarget;
+
+    [Tooltip("How far in degrees can you move the camera up")]
+    public float TopClamp = 70.0f;
+
+    [Tooltip("How far in degrees can you move the camera down")]
+    public float BottomClamp = -30.0f;
+
+    [Tooltip("Additional degress to override the camera. Useful for fine tuning camera position when locked")]
+    public float CameraAngleOverride = 0.0f;
+
+    [Tooltip("For locking the camera position on all axis")]
+    public bool LockCameraPosition = false;
+
+    private float _cinemachineTargetYaw;
+    private float _cinemachineTargetPitch;
+    private const float _threshold = 0.01f;
+
+    private bool IsCurrentDeviceMouse
     {
-        controller = GetComponent<ThirdPersonController>();
-        Reticle.enabled = false;
-        distanceText.enabled = false;
+        get
+        {
+            if (_playerInput == null) return false;
+
+            return _playerInput.currentControlScheme == "KeyboardMouse";
+        }
     }
 
-    void Update()
+
+    void Awake()
+    {
+        InstanceFinder.TimeManager.OnUpdate += TimeManager_OnUpdate;
+        InstanceFinder.TimeManager.OnLateUpdate += TimeManager_OnLateUpdate;
+
+        controller = transform.parent.GetComponent< PredictionMotor>();
+        myTarget = transform.parent.GetComponentInChildren<Targetable>();
+        Reticle.enabled = false;
+        distanceText.enabled = false;
+
+        gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        if (InstanceFinder.TimeManager != null)
+        {
+            InstanceFinder.TimeManager.OnUpdate -= TimeManager_OnUpdate;
+            InstanceFinder.TimeManager.OnLateUpdate -= TimeManager_OnLateUpdate;
+        }
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        if (base.IsOwner)
+        {
+            _playerInput = transform.parent.GetComponent<PlayerInput>();
+            _playerInput.enabled = true;
+            _input = transform.parent.GetComponent<StarterAssetsInputs>();
+
+            // TODO move this naming elsewhere
+            gameObject.name = "Player (" + ObjectId.ToString() + ")";
+
+            followCamera = Instantiate(followCameraPrefab).GetComponent<CinemachineVirtualCamera>();
+            targetingCameras = new CinemachineVirtualCamera[2];
+            targetingCameras[0] = Instantiate(targetCameraPrefab).GetComponent<CinemachineVirtualCamera>();
+            targetingCameras[1] = Instantiate(targetCameraPrefab).GetComponent<CinemachineVirtualCamera>();
+            followCamera.Follow = CinemachineCameraTarget.transform;
+
+            gameObject.SetActive(true);
+        }
+    }
+
+    public override void OnStartNetwork()
+    {
+        base.OnStartNetwork();
+
+        _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
+    }
+
+    void TimeManager_OnUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            _targetting = !_targetting;
+            _targeting = !_targeting;
 
             if (currentTarget == null)
                 FindTarget(true);
 
             CinemachineVirtualCamera cam;
-            if (_targetting)
+            if (_targeting)
             {
-                cam = targettingCameras[currentTargettingCamera];
+                cam = targetingCameras[currentTargetingCamera];
             }
             else
             {
-                controller.CameraLookAt(currentTarget.transform);
-                cam = defaultCamera;
+                CameraLookAt(currentTarget.transform);
+                cam = followCamera;
             }
-            Reticle.enabled = _targetting;
+            Reticle.enabled = _targeting;
             if (sortingType == SortingType.Distance)
-                distanceText.enabled = _targetting;
+                distanceText.enabled = _targeting;
             ChangeCamera(cam);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
-            if (_targetting)
+            if (_targeting)
             {
-                controller.CameraLookAt(currentTarget.transform);
-                ChangeCamera(defaultCamera);
-                _targetting = false;
+                CameraLookAt(currentTarget.transform);
+                ChangeCamera(followCamera);
+                _targeting = false;
                 Reticle.enabled = false;
                 distanceText.enabled = false;
             }
@@ -84,30 +176,51 @@ public class PlayerCameraController : MonoBehaviour
         }
 
         sortingLabel.text = sortingType.ToString();
+    }
 
-        if (_targetting)
+    private void TimeManager_OnLateUpdate()
+    {
+        if (base.IsOwner)
         {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
+            if (_targeting)
             {
-                FindTarget(false);
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha3))
-            {
-                FindTarget(true);
-            }
+                if (Input.GetKeyDown(KeyCode.Alpha1))
+                {
+                    FindTarget(false);
+                }
+                else if (Input.GetKeyDown(KeyCode.Alpha3))
+                {
+                    FindTarget(true);
+                }
 
-            UpdateTargetingCameraPosition();
-            _targetDistance = Vector3.Distance(transform.position, currentTarget.transform.position);
-            distanceText.text = _targetDistance.ToString("#.00");
-        }  
+                UpdateTargetingCameraPosition();
+                _targetDistance = Vector3.Distance(transform.position, currentTarget.transform.position);
+                distanceText.text = _targetDistance.ToString("#.00");
+            }
+            else
+            {
+                CameraRotation();
+            }
+        }
     }
 
     private void FindTarget(bool forward)
     {
+        // TODO temporary targetable finder, rework into target manager
+        possibleTargets = new List<Targetable>(FindObjectsOfType<Targetable>());
+        foreach (Targetable targetable in possibleTargets)
+        {
+            if (targetable.gameObject == myTarget.gameObject)
+            {
+                possibleTargets.Remove(targetable);
+                break;
+            }
+        }
+
         if (possibleTargets.Count < 1)
         {
             Debug.LogError("No targets in list");
-            _targetting = false;
+            _targeting = false;
             return;
         }
 
@@ -167,9 +280,9 @@ public class PlayerCameraController : MonoBehaviour
 
         currentTarget = possibleTargets[_targetIndex];
         // smooth quickly changing targets
-        targettingCameras[currentTargettingCamera].transform.position = Camera.main.transform.position;
-        currentTargettingCamera = WrapIndex(currentTargettingCamera + 1, targettingCameras.Length);
-        ChangeCamera(targettingCameras[currentTargettingCamera]);
+        targetingCameras[currentTargetingCamera].transform.position = Camera.main.transform.position;
+        currentTargetingCamera = WrapIndex(currentTargetingCamera + 1, targetingCameras.Length);
+        ChangeCamera(targetingCameras[currentTargetingCamera]);
     }
 
     public float targetDistance;
@@ -207,7 +320,45 @@ public class PlayerCameraController : MonoBehaviour
 
         var position = player - (direction * cameraDistance);
 
-        targettingCameras[currentTargettingCamera].LookAt = currentTarget.transform;
-        targettingCameras[currentTargettingCamera].transform.position = position;
+        targetingCameras[currentTargetingCamera].LookAt = currentTarget.transform;
+        targetingCameras[currentTargetingCamera].transform.position = position;
+    }
+
+    private void CameraRotation()
+    {
+        // if there is an input and camera position is not fixed
+        if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
+        {
+            //Don't multiply mouse input by Time.deltaTime;
+            float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+
+            _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
+            _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
+        }
+
+        // clamp our rotations so our values are limited 360 degrees
+        _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+        _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+
+        // Cinemachine will follow this target
+        CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
+            _cinemachineTargetYaw, 0.0f);
+    }
+
+    public void CameraLookAt(Transform target)
+    {
+        CinemachineCameraTarget.transform.LookAt(target);
+        _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
+        var pitch = CinemachineCameraTarget.transform.rotation.eulerAngles.x;
+        if (pitch > 180f) // fixes clamping issue in CameraRotation()
+            pitch -= 360;
+        _cinemachineTargetPitch = pitch;
+    }
+
+    private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+    {
+        if (lfAngle < -360f) lfAngle += 360f;
+        if (lfAngle > 360f) lfAngle -= 360f;
+        return Mathf.Clamp(lfAngle, lfMin, lfMax);
     }
 }
--- Assets/ThirdPersonPrediction/Script/PredictionMotor.cs
+++ Assets/ThirdPersonPrediction/Script/PredictionMotor.cs
@@ -56,25 +56,7 @@ public class PredictionMotor : NetworkBehaviour
     [Tooltip("What layers the character uses as ground")]
     public LayerMask GroundLayers;
 
-    [Header("Cinemachine")]
-    [Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
-    public GameObject CinemachineCameraTarget;
 
-    [Tooltip("How far in degrees can you move the camera up")]
-    public float TopClamp = 70.0f;
-
-    [Tooltip("How far in degrees can you move the camera down")]
-    public float BottomClamp = -30.0f;
-
-    [Tooltip("Additional degress to override the camera. Useful for fine tuning camera position when locked")]
-    public float CameraAngleOverride = 0.0f;
-
-    [Tooltip("For locking the camera position on all axis")]
-    public bool LockCameraPosition = false;
-
-    // cinemachine
-    private float _cinemachineTargetYaw;
-    private float _cinemachineTargetPitch;
 
     // player
     private float _animationBlend;
@@ -175,12 +157,6 @@ public class PredictionMotor : NetworkBehaviour
         }
     }
 
-    private void LateUpdate()
-    {
-        if (base.IsOwner)
-            CameraRotation();
-    }
-
     public override void OnStartClient()
     {
         base.OnStartClient();
@@ -189,19 +165,16 @@ public class PredictionMotor : NetworkBehaviour
 
         if (base.IsOwner)
         {
-            GameObject.FindGameObjectWithTag("PlayerFollowCamera").GetComponent<CinemachineVirtualCamera>().Follow = CinemachineCameraTarget.transform;
             _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
             _playerInput = GetComponent<PlayerInput>();
             _playerInput.enabled = true;
-            _input = GetComponent<StarterAssetsInputs>();
+            _input = GetComponent<StarterAssetsInputs>();            
         }
     }
 
     public override void OnStartNetwork()
     {
         base.OnStartNetwork();
-
-        _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
 
         _hasAnimator = TryGetComponent(out _animator);
 
@@ -297,27 +270,6 @@ public class PredictionMotor : NetworkBehaviour
         {
             _animator.SetBool(_animIDGrounded, Grounded);
         }
-    }
-
-    private void CameraRotation()
-    {
-        // if there is an input and camera position is not fixed
-        if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
-        {
-            //Don't multiply mouse input by Time.deltaTime;
-            float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
-
-            _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
-            _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
-        }
-
-        // clamp our rotations so our values are limited 360 degrees
-        _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
-        _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
-
-        // Cinemachine will follow this target
-        CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
-            _cinemachineTargetYaw, 0.0f);
     }
 
     private void MoveWithData(MoveData md, float delta)
@@ -425,13 +377,6 @@ public class PredictionMotor : NetworkBehaviour
         {
             _verticalVelocity += Gravity * delta;
         }
-    }
-
-    private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
-    {
-        if (lfAngle < -360f) lfAngle += 360f;
-        if (lfAngle > 360f) lfAngle -= 360f;
-        return Mathf.Clamp(lfAngle, lfMin, lfMax);
     }
 
     private void OnDrawGizmosSelected()
